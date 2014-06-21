7439b4a8d1040fb8238beaef1d4871674ee9f5ce
Tomek Bawej <tomek.bawej@gmail.com>
2014-06-22 00:48:25 +0200

    some visualization added. battery, contact, FFT next

---
+++ eegvisualizerwindow.cpp
@@ -0,0 +1,6 @@
+#include "eegvisualizerwindow.h"
+
+EegVisualizerWindow::EegVisualizerWindow(QWidget *parent) :
+    QWidget(parent)
+{
+}
---
+++ eegvisualizerwindow.h
@@ -0,0 +1,18 @@
+#ifndef EEGVISUALIZERWINDOW_H
+#define EEGVISUALIZERWINDOW_H
+
+#include <QWidget>
+
+class EegVisualizerWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit EegVisualizerWindow(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // EEGVISUALIZERWINDOW_H
